e301fe2040240e1bb0e66ec8bf8578a0f0ac0170
Injamamul Haque Sonet <48057930+ihsonnet@users.noreply.github.com>
2020-04-20 17:18:12 +0600

    mouse click disable

---
+++ js/lclick.js
@@ -0,0 +1,11 @@
+function disableselect(e){
+return false
+}
+function reEnable(){
+return true
+}
+document.onselectstart=new Function ("return false")
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
